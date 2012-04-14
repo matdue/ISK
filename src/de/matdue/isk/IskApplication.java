1e7e98f24c13a37dd82a8d310f1e1b5c5e474749
Matthias Düsterhöft <m.duesterhoeft@googlemail.com>
2012-04-14 19:30:06 +0200

    Made BitmapManager globally available

--- src/de/matdue/isk/IskApplication.java
+++ src/de/matdue/isk/IskApplication.java
@@ -1,21 +1,28 @@
 package de.matdue.isk;
 
+import de.matdue.isk.bitmap.BitmapManager;
 import de.matdue.isk.database.IskDatabase;
 import android.app.Application;
 
 public class IskApplication extends Application {
 	
 	private IskDatabase iskDatabase;
+	private BitmapManager bitmapManager;
 	
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		
 		iskDatabase = new IskDatabase(this);
+		bitmapManager = new BitmapManager(this, getCacheDir());
 	}
 	
 	public IskDatabase getIskDatabase() {
 		return iskDatabase;
+	}
+	
+	public BitmapManager getBitmapManager() {
+		return bitmapManager;
 	}
 
 }
