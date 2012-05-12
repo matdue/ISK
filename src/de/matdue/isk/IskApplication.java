649c0f1dbd439fd5eb420a24985cbf14a1f97e88
Matthias Düsterhöft <m.duesterhoeft@googlemail.com>
2012-05-12 21:54:51 +0200

    Set strict mode

--- src/de/matdue/isk/IskApplication.java
+++ src/de/matdue/isk/IskApplication.java
@@ -3,6 +3,8 @@ package de.matdue.isk;
 import de.matdue.isk.bitmap.BitmapManager;
 import de.matdue.isk.database.IskDatabase;
 import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.os.StrictMode;
 
 public class IskApplication extends Application {
 	
@@ -12,6 +14,23 @@ public class IskApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		
+		// Enable strict mode
+		int applicationFlags = getApplicationInfo().flags;
+		if ((applicationFlags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
+			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+					.detectDiskReads()
+					.detectDiskWrites()
+					.detectNetwork()
+					.penaltyLog()
+					.build());
+			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+					.detectLeakedClosableObjects()
+					.detectLeakedSqlLiteObjects()
+					.detectActivityLeaks()
+					.penaltyLog()
+					.build());
+		}
 		
 		iskDatabase = new IskDatabase(this);
 		bitmapManager = new BitmapManager(this, getCacheDir());
