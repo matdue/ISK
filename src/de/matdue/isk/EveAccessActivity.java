d90d2d07bef722d0f34a81be8a2a03313453d40b
Matthias Düsterhöft <m.duesterhoeft@googlemail.com>
2012-05-12 23:50:25 +0200

    Use LoaderManager

--- src/de/matdue/isk/EveAccessActivity.java
+++ src/de/matdue/isk/EveAccessActivity.java
@@ -3,51 +3,89 @@ package de.matdue.isk;
 import java.text.DateFormat;
 import java.util.Date;
 
-import android.app.ListActivity;
+import android.app.FragmentManager;
+import android.app.ListFragment;
+import android.app.LoaderManager;
 import android.content.Context;
-import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListAdapter;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
-public class EveAccessActivity extends ListActivity {
+public class EveAccessActivity extends IskActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		
-		setContentView(R.layout.eveaccess);
-		
-		IskApplication iskApp = (IskApplication) getApplication();
-		Cursor dataCursor = iskApp.getIskDatabase().getEveApiHistoryCursor();
-		startManagingCursor(dataCursor);
-		
-		ListAdapter adapter = new HistoryAdapter(this, 
-				R.layout.eveaccess_entry, 
-				dataCursor);
-		setListAdapter(adapter);
+		FragmentManager fm = getFragmentManager();
+		if (fm.findFragmentById(android.R.id.content) == null) {
+            CursorLoaderListFragment list = new CursorLoaderListFragment();
+            fm.beginTransaction().add(android.R.id.content, list).commit();
+        }
 	}
+	
+	public class CursorLoaderListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+		
+		private HistoryAdapter adapter;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			Intent intent = new Intent(this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-            return true;
+		@Override
+		public void onActivityCreated(Bundle savedInstanceState) {
+			super.onActivityCreated(savedInstanceState);
+			
+			// Give some text to display if there is no data.
+			setEmptyText(getResources().getText(R.string.eveaccess_no_data));
+			
+			adapter = new HistoryAdapter(getActivity(), 
+					R.layout.eveaccess_entry, 
+					null);
+			setListAdapter(adapter);
+			
+			// Start out with a progress indicator.
+            setListShown(false);
             
-        default:
-    		return super.onOptionsItemSelected(item);
+            // Prepare the loader. Either re-connect with an existing one, or start a new one.
+            getLoaderManager().initLoader(0, null, this);
 		}
-	}
+		
+		@Override
+		public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+			return new SimpleCursorLoader(getActivity()) {
+				@Override
+				public Cursor loadInBackground() {
+					return getDatabase().getEveApiHistoryCursor();
+				}
+			};
+		}
 
-	class HistoryAdapter extends ResourceCursorAdapter {
+		@Override
+		public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+			// Swap the new cursor in.  (The framework will take care of closing the
+            // old cursor once we return.)
+            adapter.swapCursor(data);
+
+            // The list should now be shown.
+            if (isResumed()) {
+                setListShown(true);
+            } else {
+                setListShownNoAnimation(true);
+            }		
+        }
+
+		@Override
+		public void onLoaderReset(Loader<Cursor> arg0) {
+			// This is called when the last Cursor provided to onLoadFinished()
+            // above is about to be closed.  We need to make sure we are no
+            // longer using it.
+			adapter.swapCursor(null);
+		}
+		
+	}
+	
+	static class HistoryAdapter extends ResourceCursorAdapter {
 		
 		private DateFormat dateFormatter;
 
@@ -83,7 +121,7 @@ public class EveAccessActivity extends ListActivity {
 			viewHolder.result.setText(cursor.getString(4));
 		}
 		
-		class ViewHolder {
+		static class ViewHolder {
 			TextView datetime;
 			TextView url;
 			TextView key_id;
